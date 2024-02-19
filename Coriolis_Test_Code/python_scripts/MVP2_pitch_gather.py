e2eb5d3de88a2101416cd3cd840ec6ef40f8e836
Trevor Bailey <trevorbailey@arizona.edu>
2024-02-19 16:07:19 -0700

    Yaw slow

--- Coriolis_Test_Code/python_scripts/MVP2_pitch_gather.py
+++ Coriolis_Test_Code/python_scripts/MVP2_pitch_gather.py
@@ -4,7 +4,7 @@ import serial
 ser = serial.Serial('COM19', 115200, timeout=1)
 
 try:
-    for pitch_deg in range(-25,26,1):
+    for pitch_deg in range(-25,25,1):
 
         print(f"Pitch: {pitch_deg}°")
 
@@ -17,10 +17,12 @@ try:
         ser.write(b'\xAA')
 
 
-        file_path = f"../data_files/MVP2/Yaw_slow/{pitch_deg}deg.csv"
+        file_path = f"../data_files/MVP2/Yaw_fast/{pitch_deg}deg.csv"
 
         with open(file_path, 'w') as file:
             file.write("Reading_Count,Pitch,Yaw,Pitotstatic\n")
+
+            ser.flushInput()
 
             for _ in range(200):
                 # Read a line from the serial port
